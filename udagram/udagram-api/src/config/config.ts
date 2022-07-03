8967009a1ecbe037d0e7e92239a29f038cd2b741
Mohamed Elrayes <mohamad_hesham1@hotmail.com>
2022-07-03 17:44:06 +0200

    second

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
@@ -10,13 +10,15 @@ export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port: Number(process.env.PORT),
+  port: Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
+  aws_access_key_id: AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: AWS_SECRET_ACCESS_KEY,
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
+  url: "http://hbucketh.s3-website-us-east-1.amazonaws.com",
   jwt: {
     secret: process.env.JWT_SECRET,
   },
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "contacts",
+  apiHost: "http://contacts-env.eba-qfzyfjip.us-east-1.elasticbeanstalk.com",
 };
 
 /*
