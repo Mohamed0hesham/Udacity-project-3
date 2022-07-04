a1d493718f3dbc767b0868caa3ad3d38be63d6f2
Mohamed Elrayes <mohamad_hesham1@hotmail.com>
2022-07-04 13:30:59 +0200

    edit env url

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: "contacts",
-  apiHost: "http://contacts-env.eba-qfzyfjip.us-east-1.elasticbeanstalk.com",
+  apiHost:
+    "http://contacts-env.eba-qfzyfjip.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
